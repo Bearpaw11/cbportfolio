b533683aaadb34853768d7b434d1bf8a99f4d364
Chris Behrens <chrisbehrens@yahoo.com>
2022-04-03 21:12:37 -0700

    imported all components to App.js and added them in the App function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Navbar from './components/NavBar/NavBar'
+import About from './components/About/About'
+import Skills from './componets/SKills/Skills'
+import Home from './components/Home/Home'
+import Contact from './components/Contact/Contact'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Contact/>
     </div>
   )
 }

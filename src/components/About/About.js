e60228565f235efa3a98210f148d17820cea965f
Chris Behrens <chrisbehrens@yahoo.com>
2022-04-04 15:29:57 -0700

    Added PageHeader component. This is a reusable component for all pagess. Started About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,26 @@
+import classes from './About.module.css'
+import PageHeader from './../PageHeader/PageHeader'
+import Chris from './../../images/Chris.png'
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={classes.AboutMe}>
+      <PageHeader title={'About Me'}/>
+      <div className={classes.Container}>
+        <div className={classes.Text}>
+          <h2>Hello,</h2>
+          <p>
+          I am a Full Stack Web Developer who is a critical thinker, team player and someone who loves a challenge. I have a Full Stack Web Development certificate from The University of Arizona. I enjoy front end programming and have extensive experience using React and continue to learn more and more. I have built this portfolio to help showcase my skills with front end programs such as HTML5, CSS, JavaScript, Bootstrap, React as well as back end programs and frameworks such as Node.js, express, MongoDB and MySQL.
+          <br/>
+          <br/>
+          My experience as a Systems Engineer has given me knowledge of the cutting edge hardware and software being used to accelerate growth and new technologies in this space. I also have a strong business background that includes a Bachelor’s degree in Finance that can be used to put a monetary perspective on projects. I am excited to leverage my expertise, education and knowledge to become an asset to a software development team.
+          </p>
+        </div>
+        <div className={classes.Photo}>
+          <img className={classes.Me} src={Chris} alt={"Photo of Chris"}></img>
+        </div>
+      </div>
+      
     </div>
   )
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div className={classes.Home}>
       <div className={classes.Container}>
         <h1 className={classes.Hello}>Hello, I'm Chris.</h1>
-        <h1>Welcome to my website</h1>
+        <h1>Welcome to my portfolio!</h1>
       </div>
       <img className={classes.World} src={world} alt="head img"></img> 
     </div>

bdeaf39a2341d481af048cb986427102b8ae1b23
Chris Behrens <chrisbehrens@yahoo.com>
2022-04-04 14:47:50 -0700

    completed Home component and CSS

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,15 @@
+import classes from './Home.module.css'
+import Code from '../../images/code.png'
+import world from '../../images/world.png'
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={classes.Home}>
+      <div className={classes.Container}>
+        <h1 className={classes.Hello}>Hello, I'm Chris.</h1>
+        <h1>Welcome to my website</h1>
+      </div>
+      <img className={classes.World} src={world} alt="head img"></img> 
     </div>
   )
 };

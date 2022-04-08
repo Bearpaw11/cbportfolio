d7271d63a3f02567fed053be0ba0ac0a187452fe
Chris Behrens <chrisbehrens@yahoo.com>
2022-04-08 12:55:28 -0700

    changed up the Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,15 @@
 import classes from './Home.module.css'
-import code from '../../images/code.png'
-import world from '../../images/world.png'
+import headshot from '../../images/headshot.png'
 
 const Home = () => {
   return (
     <div className={classes.Home} id="home">
       <div className={classes.Container}>
         <h1 className={classes.Hello}>Hello World.</h1>
-        <h1>Welcome to my portfolio!</h1>
+        <img className={classes.Headshot} src={headshot} alt="headshot img"></img>
+        <h1>Chris Behrens</h1>
+        <h2>~Web Developer~</h2>
       </div>
-      <img className={classes.World} src={world} alt="world img"></img> 
-      <img className={classes.code} src={code} alt="code img"></img>
     </div>
   )
 };

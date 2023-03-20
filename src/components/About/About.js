2992579cf57c55fdab5dd38cbc5624287d6574c1
Chris Behrens <chrisbehrens@yahoo.com>
2023-03-20 10:48:29 -0700

    updated about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
         <div className={classes.Text}>
           <h2>Hello, I'm Chris</h2>
           <p>
-          I am a Full Stack Web Developer who is a critical thinker, team player and someone who loves a challenge. I have a Full Stack Web Development certificate from The University of Arizona and TEKsystems. I have built this portfolio to help showcase my skills Java, HTML5, CSS, JavaScript, Bootstrap, React, MySQL, and Snowflake. 
+          I am a Full Stack Web Developer who is a critical thinker, team player and someone who loves a challenge. I have a Full Stack Web Development certificate from The University of Arizona and TEKsystems. I have built this portfolio to help showcase my skills which include Java, HTML5, CSS, JavaScript, Bootstrap, React, MySQL, and Snowflake. 
           <br/> 
           <br/>
           I have experience working as a sofware developer at United Health Group where I developed and maintained our BCP mirroring program which mirrors over 50 billion rows of data from our on prem SQL Server to Snowflake. My experience as a Systems Engineer has given me knowledge of the cutting edge hardware and software being used to accelerate growth and new technologies in this space. I also have a strong business background that includes a Bachelor’s degree in Finance that can be used to put a monetary perspective on projects. I am excited to leverage my expertise, education and knowledge to become an asset to a software development team.

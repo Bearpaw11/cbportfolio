1d42f3ffe48efc4d6e966dfc416eab998a9db0b7
Chris Behrens <chrisbehrens@yahoo.com>
2023-04-04 10:55:23 -0700

    updated Home section and added resume

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,11 @@
 import classes from './Home.module.css'
 import headshot from '../../images/headshot.png'
+import resume from '../../images/resume.pdf'
+import { AiOutlineFilePdf } from "react-icons/ai";
+
+const handleURL = (url) => {
+  return () => window.open(url, "_blank");
+};
 
 const Home = () => {
   return (
@@ -8,7 +14,11 @@ const Home = () => {
         <h1 className={classes.Hello}>Hello World.</h1>
         <img className={classes.Headshot} src={headshot} alt="headshot img"></img>
         <h1>Chris Behrens</h1>
-        <h2>~Web Developer~</h2>
+        <h2>Full-Stack Engineer</h2>
+        <div className={classes.Resume} onClick={handleURL(resume)}>
+          <h5><AiOutlineFilePdf 
+              style={{ marginBottom: "-1.5%" }}/>   Full Resume</h5>
+        </div>
       </div>
     </div>
   )

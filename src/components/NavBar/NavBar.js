7fe9094c2adf3ef33a8f6f60f2ab2059664b5acb
Chris Behrens <chrisbehrens@yahoo.com>
2023-04-04 12:48:27 -0700

    updated Navbar, added experience

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -12,10 +12,13 @@ const NavBar = () => {
             <a href="/#about">About</a>
           </li>
           <li>
-            <a href="/#skills">Skills</a>
+            <a href="/#experience">Experience</a>
           </li>
           <li>
             <a href="/#projects">Projects</a>
+          </li>
+          <li>
+            <a href="/#skills">Skills</a>
           </li>
           <li>
             <a href="/#contact">Contact</a>

961a42848faab33760b9920474103c73095c8df7
Bearpaw11 <chrisbehrens@yahoo.com>
2024-01-24 10:59:37 -0700

    updated about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,10 +10,10 @@ const About = () => {
         <div className={classes.Text}>
           <h2>Hello, I'm Chris</h2>
           <p>
-          I am a Full Stack Web Developer who is a critical thinker, team player and someone who loves a challenge. I have a Full Stack Web Development certificate from The University of Arizona and TEKsystems. I have built this portfolio to help showcase my skills which include Java, HTML5, CSS, JavaScript, Bootstrap, React, MySQL, and Snowflake. 
+          As a seasoned Full Stack Web Developer, I bring a unique blend of critical thinking, teamwork, experience working in an agile environment, and a passion for challenges. Armed with a Full Stack Web Development certificate from The University of Arizona and TEKsystems, my skill set encompasses Java, Spring Boot, HTML5, CSS, JavaScript, Bootstrap, React, MySQL, Postgres, and Snowflake.
           <br/> 
           <br/>
-          I have experience working as a sofware developer at United Health Group where I developed and maintained our BCP mirroring program which mirrors over 50 billion rows of data from our on prem SQL Server to Snowflake. My experience as a Systems Engineer has given me knowledge of the cutting edge hardware and software being used to accelerate growth and new technologies in this space. I also have a strong business background that includes a Bachelor’s degree in Finance that can be used to put a monetary perspective on projects. I am excited to leverage my expertise, education and knowledge to become an asset to a software development team.
+          I have experience working as a Software Developer at Deloitte, contributing to a project for the IRS where I created and maintained API endpoints for seamless communication among microservices. During my tenure at United Health Group, I played a key role in developing and maintaining our BCP mirroring program, responsible for mirroring over 50 billion rows of data from our on-prem SQL Server to Snowflake. Additionally, my role as a Systems Engineer at TD Synnex has equipped me with knowledge of cutting-edge hardware and software, fostering growth and the implementation of new technologies in this space. Complementing my technical expertise is a strong business background, highlighted by a Bachelor’s degree in Finance, which enables me to provide a monetary perspective on projects.
           </p>
         </div>
         {/* <div className={classes.Photo}>

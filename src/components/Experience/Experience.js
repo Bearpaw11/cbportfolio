60adbba00ba0f73289cc00e1d1934a17024ab11a
Bearpaw11 <chrisbehrens@yahoo.com>
2024-01-24 09:34:54 -0700

    updated experience for Deloitte

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -2,6 +2,7 @@ import classes from "./Experience.module.css";
 import PageHeader from "../PageHeader/PageHeader";
 import optum from '../../images/optum.png'
 import TD from '../../images/TD.png'
+import deloitte from '../../images/Deloitte.png'
 
 const Apps = (thumbnail, title, jobTitle, description, siteLink) => {
   return (
@@ -18,6 +19,11 @@ const Apps = (thumbnail, title, jobTitle, description, siteLink) => {
     </div>
   )
 }
+
+const DeloitteTitle ="Deloitte"
+const DeloitteJobTitle = "Software Engineer"
+const DeloitteDescription="Java development using JDK 11 and Spring Boot. Create and maintain API endpoints for seamless communication among microservices. Work in an Agile environment, adapting quickly to changing project requirements for iterative solutions."
+const DeloitteLink= <a href="https://www2.deloitte.com/us/en/pages/about-deloitte/articles/technology-consulting-engineering-advantage.html?id=us:2ps:3gl:firmfy24:eng:greendot:110122:em:na:N6RBN2tG:1294776198:656188483654:p:Generic_Engineering-Advantage-Consulting:Generic_EA-Consulting-Tech_Phrase:nb&gad_source=1&gclid=Cj0KCQiAh8OtBhCQARIsAIkWb6_UMgFUyncGzQJ6qo6fRWF-_6ZbY0konad5BvPYoMVd6RFqj_vbueMaAk8EEALw_wcB" target="_blank">Deloitte.com</a>
 
 const optumTitle ="United Heath Group/Optum"
 const optumJobTitle = "Full Stack Engineer"
@@ -38,6 +44,7 @@ const Projects = () => {
       <PageHeader title={"Experience"} />
       <div className={classes.Content}>
         <div className={classes.Apps}>
+          {Apps(deloitte, DeloitteTitle, DeloitteJobTitle, DeloitteDescription, DeloitteLink)}
           {Apps(optum, optumTitle, optumJobTitle, optumDescription, optumLink)}
           {Apps(TD, TDTitle, TDJobTitle, TDdescription, TDlink)}
         </div>
